ddc4ade92e5620b685c5edda9536f5ae3ea5bf18
colo-kimjuho <hoya@colosseum.kr>
2023-03-15 18:16:22 +0900

    test

--- src/main/java/hello/AutoConfigApplication.java
+++ src/main/java/hello/AutoConfigApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutoConfigApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AutoConfigApplication.class, args);
     }
